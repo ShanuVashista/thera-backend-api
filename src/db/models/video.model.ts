7ed5d7033e0fc4a5ca3ad3bd70a98b400282de13
Pratyush <pratyushrajsingh22@gmail.com>
2022-06-24 15:58:55 +0530

    video model created

---
+++ src/db/models/video.model.ts
@@ -0,0 +1,58 @@
+import mongoose,{Schema,model,PopulatedDoc} from "mongoose";
+import {IUser} from "./user";
+
+
+export interface IVideo {
+    isYoutube: boolean;
+    title: string;
+    url: string;
+    uploaderId:PopulatedDoc<IUser>;
+    patients: Array<string>
+}
+
+
+const videoSchema = new mongoose.Schema<IVideo>(
+    {
+        isYoutube:{
+            type:Boolean
+        },
+        title:{
+            type:String,
+            required: true
+        },
+        url:{
+            type:String,
+            required: true
+        },
+        uploaderId:{
+            type: Schema.Types.ObjectId,
+            ref:'user'
+        },
+        patients:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+                default: null,
+            }
+        ]
+    },
+    {
+        timestamps:true, toJSON:{virtuals: true}
+    }
+)
+
+
+videoSchema.virtual("uploader", {
+    ref: 'user',
+    localField: "uploaderId",
+    foreignField: "_id",
+    justOne: true,
+})
+
+
+const video = model(
+    "video",
+    videoSchema
+)
+
+export default video;
